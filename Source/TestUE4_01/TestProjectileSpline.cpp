f1ad48685657b41abe878feaab5d24a67a14dea2
egoquat <egoquat@users.noreply.github.com>
2021-08-12 17:52:45 +0900

    Update TestProjectileSpline.cpp

--- Source/TestUE4_01/TestProjectileSpline.cpp
+++ Source/TestUE4_01/TestProjectileSpline.cpp
@@ -10,14 +10,20 @@ ATestProjectileSpline::ATestProjectileSpline()
 
 void ATestProjectileSpline::InitProjectile(const FVector& InPosition, FVector& InDirection, float InScale, bool bIsHitReflect, float InTimeDestroy, FColor InColor)
 {
-	SplineComponent->AddSplineWorldPoint(InPosition);
+	GWarn->Logf(ELogVerbosity::Display, TEXT(" AddSplineWorldPoint(%s)"), *InPosition.ToString());
+
+	SplineComponent->ClearSplinePoints();
+	SplineComponent->AddSplineLocalPoint(InPosition);
 	const int countIteration = 10;
 	const float distance = DEFAULT_DIST_PROJECTILESPLINE, distanceUnit = distance / (float)countIteration, distanceWave = 50.0f;
 	for (int i = 0; i < countIteration; ++i)
 	{
 		float currentDistance = distanceUnit * i;
-		FVector currentPosition = InPosition + (InDirection * currentDistance) + ((0 == i % 2)? distanceWave : -distanceWave);
-		SplineComponent->AddSplineWorldPoint(currentPosition);
+		FVector currentPosition = InPosition + (InDirection * currentDistance);
+		currentPosition.Z = currentPosition.Z + ((0 == i % 2) ? distanceWave : -distanceWave);
+		SplineComponent->AddSplineLocalPoint(currentPosition);
+
+		GWarn->Logf(ELogVerbosity::Display, TEXT(" AddSplineWorldPoint(%s)"), *currentPosition.ToString());
 	}
 
 	TimeCumulative = 0.0f;
@@ -28,7 +34,6 @@ void ATestProjectileSpline::InitProjectile(const FVector& InPosition, FVector& I
 void ATestProjectileSpline::UpdateLocation(const FVector& LocationCurrent, float DeltaTime)
 {
 	float time = TimeCumulative / (DEFAULT_DIST_PROJECTILESPLINE/DEFAULT_SPEED_PROJECTILE);
-
 	FVector newLocation = SplineComponent->GetLocationAtTime(time, ESplineCoordinateSpace::World);
 	SetActorLocationAndRotation(newLocation, Rotation, false, 0, ETeleportType::None);
 
